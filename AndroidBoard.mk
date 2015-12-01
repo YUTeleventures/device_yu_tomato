e9b6655e75f7300aa35afffe0269df5218d25c4f
Louis Popi <theh2o64@gmail.com>
2015-12-01 12:25:04 +0100

    tomato: Remove DT task
    Signed-off-by: AudioGod <audiogod@sungsonic.co>
    
    Conflicts:
    	AndroidBoard.mk

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -16,8 +16,6 @@ LOCAL_PATH := $(call my-dir)
 # limitations under the License.
 #
 
-BOARD_KERNEL_SEPARATED_DT := true
-KERNEL_DIR := kernel/yu/msm8916_64
 
 -include $(TOP)/$(KERNEL_DIR)/AndroidKernel.mk
 
@@ -26,26 +24,5 @@ KERNEL_DIR := kernel/yu/msm8916_64
 .PHONY: $(PRODUCT_OUT)/kernel
 $(PRODUCT_OUT)/kernel: $(TARGET_PREBUILT_KERNEL)
 	cp $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/kernel
-
-DTB_FILES := $(wildcard $(TOP)/$(KERNEL_DIR)/arch/arm64/boot/*.dtb)
-DTB_FILE := $(addprefix $(KERNEL_OUT)/arch/arm64/boot/,$(patsubst %.dts,%.dtb,$(call DTS_FILE,$(1))))
-ZIMG_FILE := $(addprefix $(KERNEL_OUT)/arch/arm64/boot/,$(patsubst %.dts,%-zImage,$(call DTS_FILE,$(1))))
-KERNEL_ZIMG := $(KERNEL_OUT)/arch/arm64/boot/Image
-
-define append-dtb
-mkdir -p $(KERNEL_OUT)/arch/arm64/boot;\
-$(foreach d, $(DTB_FILES), \
-    cat $(KERNEL_ZIMG) $(call DTB_FILE,$(d)) > $(call ZIMG_FILE,$(d));)
-endef
-
-## Build and run dtbtool
-DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbToolYU$(HOST_EXECUTABLE_SUFFIX)
-INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
-$(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
-	@echo -e ${CL_CYN}"Start DT image: $@"${CL_RST}
-	$(call append-dtb)
-	$(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
-	$(hide) $(DTBTOOL) -o $(INSTALLED_DTIMAGE_TARGET) -s 2048 -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/arm64/boot/dts/
-	@echo -e ${CL_CYN}"Made DT image: $@"${CL_RST}
 
 include device/qcom/msm8916_64/AndroidBoard.mk
