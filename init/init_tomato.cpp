d7193835d63b2a76569d9d32d36a91d54f9326c1
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-04-30 08:30:29 +0800

    tomato: Use the appropriate dalvik configs
    xhdpi for 720p, xxhdpi for 1080p.
    
    Change-Id: If9c1af4aa787d9b439bad8a6d41ff24aaefedbc2

--- init/init_tomato.cpp
+++ init/init_tomato.cpp
@@ -75,11 +75,26 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.bt.name", "YU5510");
         property_set("ro.hotspot.name", "YUREKA-YU5510");
         property_set("ro.direct.name", "YUREKA");
+	// Dalvik
+        property_set("dalvik.vm.heapstartsize", "16m");
+        property_set("dalvik.vm.heapgrowthlimit", "192m");
+        property_set("dalvik.vm.heapsize", "512m");
+        property_set("dalvik.vm.heaptargetutilization", "0.75");
+        property_set("dalvik.vm.heapminfree", "2m");
+        property_set("dalvik.vm.heapminfree", "8m");
     } else {
         property_set("ro.product.model", "AO5510");
         property_set("ro.bt.name", "AO5510");
         property_set("ro.hotspot.name", "YUREKA-AO5510");
         property_set("ro.direct.name", "YUREKA+");
+	// Dalvik
+        property_set("ro.product.model", "AO5510");
+        property_set("dalvik.vm.heapstartsize", "8m");
+        property_set("dalvik.vm.heapgrowthlimit", "192m");
+        property_set("dalvik.vm.heapsize", "512m");
+        property_set("dalvik.vm.heaptargetutilization", "0.75");
+        property_set("dalvik.vm.heapminfree", "512k");
+        property_set("dalvik.vm.heapminfree", "8m");
     }
 }
 
