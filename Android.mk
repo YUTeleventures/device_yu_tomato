8f15b16e40ba1c9a1a856d55d5984c121391f911
Brijesh K S <brijesh.ks@micromaxinfo.com>
2016-05-07 13:59:29 +0530

    test commit 3
    Change-Id: I166619c645f9f36d3f96b687f831a4b4aa9652a2
    Signed-off-by: Brijesh K S <brijesh.ks@micromaxinfo.com>

--- Android.mk
+++ Android.mk
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+# Test commit BrijeshMMX
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),tomato)
