57ece979abde8b3adc7fd2d918e35f3cef83af59
Louis Popi <theh2o64@gmail.com>
2016-05-08 18:21:42 +0200

    tomato: Add libtinyxml
    Change-Id: Ib54bcf98391a104693c4b8686f9e1e1a448551e3

--- device.mk
+++ device.mk
@@ -74,6 +74,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ro.product.model=YUREKA
 
+# Misc
+PRODUCT_PACKAGES += \
+    libtinyxml
+
 # Power HAL
 PRODUCT_PACKAGES += \
     power.msm8916 \
