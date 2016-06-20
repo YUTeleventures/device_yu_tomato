fdd1c8f3dc656cd2a62a3b5253424e6969929ab5
tarun93 <tarunmyid@gmail.com>
2016-06-20 14:32:40 +0530

    redirect to yu instead of cyanogen
    Change-Id: Ibc8972549f34e27c654f964795d6a0e6dcd52b38
    Signed-off-by: tarun93 <tarunmyid@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
--include device/cyanogen/msm8939-common/BoardConfigCommon.mk
+-include device/yu/msm8939-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/yu/tomato
 
