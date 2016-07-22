1343a9f9c4ad98669a937c4c0952d5b5300208b0
Jose Adolfo Galdamez <josegalre@pac-rom.com>
2016-07-21 22:15:04 -0600

    tomato: fix dalvik typo on init
    Change-Id: Ia87ff39c586aaff1b03bde248507eb1dbf1eaa85

--- init/init_tomato.cpp
+++ init/init_tomato.cpp
@@ -81,7 +81,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("dalvik.vm.heapsize", "512m");
         property_set("dalvik.vm.heaptargetutilization", "0.75");
         property_set("dalvik.vm.heapminfree", "2m");
-        property_set("dalvik.vm.heapminfree", "8m");
+        property_set("dalvik.vm.heapmaxfree", "8m");
     } else {
         property_set("ro.product.model", "AO5510");
         property_set("ro.bt.name", "AO5510");
@@ -94,7 +94,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("dalvik.vm.heapsize", "512m");
         property_set("dalvik.vm.heaptargetutilization", "0.75");
         property_set("dalvik.vm.heapminfree", "512k");
-        property_set("dalvik.vm.heapminfree", "8m");
+        property_set("dalvik.vm.heapmaxfree", "8m");
     }
 }
 
